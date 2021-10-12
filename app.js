59f7f32c727d858340ec03b792e0a1fe0242543f
kia mangrum <kia1016@gmail.com>
2021-10-12 19:09:48 -0400

    JSVariablesEx

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ console.log("Hello World!\n==========\n");
 console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
-/*
+
 // Exercise 1
 const firstName = "Kia";
 let lastName = "Mangrum";
@@ -20,74 +20,6 @@ let city = "Cleveland";
 let pastime = "painting";
 myStory = `Hello! My name is ${fullName}.  I live in ${city}.  I enjoy ${pastime} and coding!`;
 console.log(myStory);
-*/
 
 
-// JavaScript Operators Exercise
-// Exercise 1
 
-let a = 20
-let b = 4
-let add = 20 + 4;
-console.log(add);
-
-let minus = 20 - 4;
-console.log(minus);
-
-let multiply = 20 * 4;
-console.log(multiply);
-
-let divide = 20 / 4;
-console.log(divide);
-
-//Exercise 2
-
-let c = 11;
-let str = "11";
-let str2 = "eleven";
-let isPresent = true;
-let firstName2 = "Jackie";
-let lastName2 = "Chan";
-
-//What is the value of: c + str?
-console.log(c + str);
-
-//What is the value of: c + str2?
-console.log(c + str2);
-
-//What is the value of: c + isPresent?
-console.log(c + isPresent);
-
-//What is the value of: c + firstName2?
-console.log(c + firstName2);
-
-//What is the value of: c + lastName2?
-console.log(c + lastName2);
-
-//Exercise 3
-
-let d = 5;
-let str3 = "5";
-let str4 = "five";
-let isPresent2 = false;
-let firstName3 = "Robin";
-let lastName3 = "Williams";
-
-//What is the value of: d == str3?
-console.log( d == str3);
-//What is the value of: d === str3?
-console.log(d === str3);
-//What is the value of: !isPresent2?
-console.log(!isPresent2);
-//What is the value of: (“eleven” == str4 && d >= str3)?
-console.log("`eleven` == str3 && d>= str4: ", `eleven` == str4 && d>= str3);
-//What is the value of: (!isPresent2 || isPresent2)?
-console.log((!isPresent2 || isPresent2))
-//What is the value of: 0 == false?
-console.log(0 == false);
-//What is the value of: 0 === false?
-console.log(0 === false);
-//What is the value of: 0 != false?
-console.log(0 != false);
-//What is the value of 0 !== false?
-console.log(0 !== false);
